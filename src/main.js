60796cd1f1591506209241ef745010a98dac32eb
Даниил Тюкин <tyukin.01@mail.ru>
2024-02-04 18:54:01 +0300

    исправил ошибку

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import {render, RenderPosition} from './render.js';
-import HeaderInfoView from './view/header-info-view.js';
-import ListFilterView from './view/list-filter-view.js';
+import {RenderPosition} from './render.js';
 import EventsPresenter from './presenter/events-presenter.js';
+import FiltersPresenter from './presenter/filters-presenter.js';
+import HeaderInfoPresenter from './presenter/header-info-presenter.js';
 
-const siteHeaderElement = document.querySelector('.page-header');
-const siteHeaderMainElement = siteHeaderElement.querySelector('.trip-main');
-const filterElement = siteHeaderElement.querySelector('.trip-controls__filters');
-const tripEventsContainer = document.querySelector('.trip-events');
-const eventsPresenter = new EventsPresenter({eventsContainer: tripEventsContainer});
-
-render(new HeaderInfoView(), siteHeaderMainElement, RenderPosition.AFTERBEGIN);
-render(new ListFilterView(), filterElement);
+const tripMainElement = document.querySelector('.trip-main');
+const tripControlsFilters = document.querySelector('.trip-controls__filters');
+const tripEvents = document.querySelector('.trip-events');
+const eventsPresenter = new EventsPresenter({container: tripEvents});
+const filtersPresenter = new FiltersPresenter({container: tripControlsFilters});
+const headerInfoPresenter = new HeaderInfoPresenter({container: tripMainElement, place: RenderPosition.AFTERBEGIN});
 
 eventsPresenter.init();
+filtersPresenter.init();
+headerInfoPresenter.init();
